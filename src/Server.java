164fda1e496da029af100429daa66efe1ba57621
AlNualD <nuald@outlook.com>
2020-01-19 12:23:24 +0300

    server init

--- src/Server.java
+++ src/Server.java
@@ -4,9 +4,20 @@ import  java.io.*;
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8000);
+        int counter = 0;
+        while (true)
+        {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client accepted " + (counter++));
 
-        serverSocket.accept();
+            OutputStreamWriter writer =  new OutputStreamWriter(clientSocket.getOutputStream());
+            writer.write("<h1>header1</h1>");
+            writer.flush();
+            clientSocket.close();
 
-        serverSocket.close();
+
+        }
+    //    serverSocket.close();
+
     }
 }
