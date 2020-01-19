c07c52b68ef394b9b9f4a68420b8fcffbf797f6f
AlNualD <nuald@outlook.com>
2020-01-19 13:21:25 +0300

    new structure

---
+++ server/src/chat/server/ChatServer.java
@@ -0,0 +1,12 @@
+package chat.server;
+import java.io.*;
+import  java.net.*;
+
+
+public class ChatServer {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = new ServerSocket(8000);
+        
+    }
+
+}
