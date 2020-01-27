ebfcc0b7ee3fc06d895d96cf3f231ee895d552d5
AlNualD <nuald@outlook.com>
2020-01-27 14:44:00 +0300

    initWindow

--- client/src/chat/client/ClientWin.java
+++ client/src/chat/client/ClientWin.java
@@ -1,11 +1,38 @@
 package chat.client;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.net.*;
+import java.io.*;
+
+import network.pckg.*;
+
 /**
  * @author nuald
  * Основной класс для клиентского окна
  */
-public class ClientWin {
+public class ClientWin extends Application {
     public static void main(String[] args) {
+        new ClientWin();
+        Application.launch();
 
     }
+    public ClientWin(){
+
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/primaryStage.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
+
 }
