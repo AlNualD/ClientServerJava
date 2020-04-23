30cc3184d3e0b8dac1a5dd70e7b95f2c6af9cda1
AlNualD <nuald@outlook.com>
2020-04-23 08:48:41 +0300

    Update serverCommandsTests.java

--- server/src/server/tests/serverCommandsTests.java
+++ server/src/server/tests/serverCommandsTests.java
@@ -5,9 +5,18 @@ import chat.server.commands;
 import junit.framework.TestCase;
 
 public class serverCommandsTests extends TestCase {
-  public void testCommandController() {
+  public void testLoginCommand() {
     CommandsController commandsController = new CommandsController();
     assertEquals(commandsController.parseMSG("##LOGIN##sahdfsjkdfh"), commands.LOGIN);
+  }
+
+  public void testSendAllCommand() {
+    CommandsController commandsController = new CommandsController();
     assertEquals(commandsController.parseMSG("##SEND_ALL##asdfdf"), commands.SEND_ALL);
+  }
+
+  public void testWrongCommand() {
+    CommandsController commandsController = new CommandsController();
+    assertEquals(commandsController.parseMSG("##AFHG##sadfsdf"), commands.ERROR);
   }
 }
